6d72a7dd81882387cfc868ef74a84cae11395357
ashishmanchanda <ashish.manchanda@paytm.com>
2021-10-01 10:16:56 +0530

    aDDED CODE

---
+++ src/main/java/codingpatterns/slidingwindow/FruitsIntobasket.java
@@ -0,0 +1,36 @@
+package codingpatterns.slidingwindow;
+
+import java.util.*;
+class Solution {
+    public static int totalFruit(int[] tree) {
+        if (tree == null || tree.length == 0) {
+            return 0;
+        }
+
+        Map<Integer, Integer> hm = new HashMap<>();
+        int s = 0, e = 0, result = 0;
+
+        while (e < tree.length) {
+            hm.put(tree[e], hm.getOrDefault(tree[e], 0) + 1);
+            ++e;
+
+            while (hm.size() > 2) {
+                hm.put(tree[s], hm.get(tree[s]) - 1);
+                if (hm.get(tree[s]) == 0) {
+                    hm.remove(tree[s]);
+                }
+                ++s;
+            }
+
+            result = Math.max(result, e - s);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] tree={1,2,3,4};
+        System.out.println(totalFruit(tree));
+    }
+}
+
