622afcc2097d44a236868a92ec524ec239bb9f06
ashishmanchanda <ashish.manchanda@paytm.com>
2021-10-02 12:29:36 +0530

    aDDED CODE

---
+++ src/main/java/Problem.java
@@ -0,0 +1,19 @@
+public class Problem {
+
+    /*static int findCount(int []a){
+        int result=0;
+        boolean inc;
+        boolean desr;
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int [] a={1,2,3,4\,5,6,7,0,2,1};
+
+
+
+
+
+    }*/
+}
---
+++ src/main/java/Problem2.java
@@ -0,0 +1,32 @@
+public class Problem2 {
+    static int result  = 0;
+    static int maxDiff = Integer.MAX_VALUE;
+
+    static int sqrt(int num, int mid,int prevMid) {
+        if (mid <= 1 || mid >= num) {
+            return result;
+        }
+        System.out.println("mid"+mid);
+        if (Math.abs(mid * mid - num) < maxDiff) {
+            maxDiff=Math.abs(mid * mid - num);
+            result = mid;
+        }
+        if (mid * mid == num) {
+            result=mid;
+            return result;
+        } else if (mid * mid < num) {
+           result= sqrt(num, (mid + prevMid) / 2,mid);
+        } else {
+           result = sqrt(num, mid / 2,mid);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int num = 9;
+        int mid = num / 2;
+        int prevmid=num;
+        System.out.println(sqrt(num, mid,prevmid));
+
+    }
+}
