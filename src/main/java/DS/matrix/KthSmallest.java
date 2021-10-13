0b646369bd5ca3702f82bcd2966ae83575600169
ashishmanchanda <ashish.manchanda@paytm.com>
2021-10-13 11:15:02 +0530

    Added more classes

---
+++ src/main/java/DS/matrix/KthSmallest.java
@@ -0,0 +1,29 @@
+package DS.matrix;
+
+import java.util.*;
+
+class KthSmallestInSortedMatrix {
+
+    public static int findKthSmallest(int[][] matrix, int k) {
+        // TODO: Write your code here
+        int n = matrix.length;
+        int len = matrix[0].length;
+        Queue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < len; j++) {
+                if (queue.size() == k) {
+                    int num = queue.poll();
+                    if (matrix[i][j] < num) queue.add(matrix[i][j]);
+                    else queue.add(num);
+                } else queue.add(matrix[i][j]);
+            }
+        }
+        return queue.peek();
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = { { 2, 6, 8 }, { 3, 7, 10 }, { 5, 8, 11 } };
+        int result = KthSmallestInSortedMatrix.findKthSmallest(matrix, 5);
+        System.out.print("Kth smallest number is: " + result);
+    }
+}
---
+++ src/main/java/algo/DP/lcspattern/LIS.java
@@ -0,0 +1,25 @@
+package algo.DP.lcspattern;
+
+class LIS {
+    public int findLISLength(int[] nums) {
+        return findLISLengthRecursive(nums, 0, -1);
+    }
+    private int findLISLengthRecursive(int[] nums, int currentIndex, int previousIndex){
+if(currentIndex == nums.length)
+return 0;
+    // include nums[currentIndex] if it is larger than the last included number
+    int c1 = 0;
+if(previousIndex == -1 || nums[currentIndex] > nums[previousIndex])
+    c1 = 1 + findLISLengthRecursive(nums, currentIndex+1, currentIndex);
+    // excluding the number at currentIndex
+    int c2 = findLISLengthRecursive(nums, currentIndex+1, previousIndex);
+return Math.max(c1, c2);
+}
+    public static void main(String[] args) {
+        LIS lis = new LIS();
+        int[] nums = {4,2,3,6,10,1,12};
+        System.out.println(lis.findLISLength(nums));
+        nums = new int[]{-4,10,3,7,15};
+        System.out.println(lis.findLISLength(nums));
+    }
+}
